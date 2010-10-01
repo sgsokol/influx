1d7e37314fe9c120a7c5c8703a604f5fc8c59006
Serguei Sokol <sokol@insa-toulouse.fr>
2010-10-01 13:25:29 +0000

    fixed ftbl argument processing;

--- influx_s.sh
+++ influx_s.sh
@@ -4,18 +4,29 @@
 # - compile .f in .so
 # - start generated R script for minimization
 #
-# usage: ./influx_s.sh network[.ftbl] [...]
+# usage:
+# ./influx_s.sh [...] network[.ftbl]
 # optional extra params [...] are passed as is to R script
-if [ $# = 0 ]; then
-   echo "usage: ./influx_s.sh network[.ftbl] [opt params to R script]";
+n=$#
+if [ $n = 0 ]; then
+   echo "usage: ./influx_s.sh [opt_params_to_R_script] network[.ftbl]";
    exit 1;
 fi
 
 echo "code gen:" $(date);
 direx=$(dirname "$0");
-f="$1"
-shift;
-eargs="$@"
+
+# test which argument is ftbl file, first or last
+f="${1%%.ftbl}"
+if [ -r "$f".ftbl ]; then
+   shift;
+   eargs="$@";
+else
+   f="${@:n:1}";
+   f="${f%%.ftbl}";
+   n=$((n-1));
+   eargs="$@:1:n";
+fi
 
 $direx/ftbl2optR.py "$f" &&
    echo "compil  :" $(date) &&
