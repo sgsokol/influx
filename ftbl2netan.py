fac4d36f83d1e251e72b24934a3867147651f9bd
Serguei Sokol <sokol@insa-toulouse.fr>
2012-09-25 13:33:05 +0000

    added rcumo2i0 and emu2i0 converters

--- ftbl2netan.py
+++ ftbl2netan.py
@@ -50,7 +50,9 @@ if __name__ == "__main__":
     else:
         tools_ssg.dict2kvh({"rcumo_sys": C13_ftbl.rcumo_sys(netan)});
     tools_ssg.dict2kvh({"vrcumo": netan["vrcumo"]});
+    tools_ssg.dict2kvh({"rcumo2i0": netan["rcumo2i0"]});
     tools_ssg.dict2kvh({"rcumo_input": netan["rcumo_input"]});
     tools_ssg.dict2kvh({"vemu_input": netan["vemu_input"]});
     tools_ssg.dict2kvh({"vemu": netan["vemu"]});
+    tools_ssg.dict2kvh({"emu2i0": netan["emu2i0"]});
     sys.exit(0);
