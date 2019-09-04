f02dd95aa7f8835961959dcc5a4f1e9d72870552
Serguei Sokol <sokol@insa-toulouse.fr>
2019-09-04 14:57:17 +0200

    added some helper files

---
+++ clean_ftbl.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# clean files after runing influx_s[i] in the current (default) or a directory given in first and next parameters
+
+if [ $# != 0 ]; then
+   dirs="$@"
+else
+   dirs="."
+fi
+
+for d in $dirs; do
+   ( cd "$d" &&
+   for f in *.ftbl; do
+      #echo fb="${f/.ftbl/}"
+      fb="${f/.ftbl/}"
+      rm -f "$fb"{.R,.err,.log,.pres.csv,.RData,_res.kvh,_ires.kvh,.netan,.sif,.xgmml,_netan.kvh} *.$fb.attrs {edge,node}.*.$fb
+   done )
+done
