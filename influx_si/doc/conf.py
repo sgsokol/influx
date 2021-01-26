0e03914a396bb866f6f1d90935eceb6ab052fda5
Sokol Serguei <sokol@insa-09501>
2021-01-26 11:30:54 +0100

    fixed copyright holder
    Signed-off-by: Sokol Serguei <sokol@insa-09501>

--- influx_si/doc/conf.py
+++ influx_si/doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'influx_si'
-copyright = u'2012-2020, INRAE'
+copyright = u'2012-2020, INRAE/INSA/CNRS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
